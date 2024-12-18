913882e4611a9cfe479e0385b02460ef42855ad7
zalfamutia03 <zalfamutia03@gmail.com>
2024-12-18 10:00:47 +0700

    commit satu

---
+++ jumlah_bilangan.py
@@ -0,0 +1,9 @@
+n = int(input("Masukkan nilai n: "))
+total = 0
+i = 1
+
+while i <= n:
+    total += i
+    i += 1
+    
+print(f"Jumlah bilangan dari 1 hingga {n} adalah: {total}")
---
+++ luas_lingkaran.py
@@ -0,0 +1,5 @@
+import math
+r = float(input('Masukkan jari-jari lingkaran:'))
+
+luas = math.pi *r*r
+print(f"Luas lingkaran dengan jari-jari {r} adalah: {luas}")
